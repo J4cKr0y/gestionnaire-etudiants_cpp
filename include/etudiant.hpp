57735942a6cca1bbee145dd51bf266f9a8d5a942
Jacky Royer <kiky270281@gmail.com>
2025-10-22 22:55:02 +0200

    feat: gestionnaire des étudiants

---
+++ include/etudiant.hpp
@@ -0,0 +1,18 @@
+﻿#ifndef ETUDIANT_H
+#define ETUDIANT_H
+
+#include <iostream>
+
+struct etudiant {
+    int matricule;
+    int age;
+    char nom[50];
+};
+
+
+void initialiser_etudiant(etudiant* e, int mat, int a, const char* n);
+void afficher_etudiant(const etudiant* e);
+etudiant* creer_etudiant(int mat, int a, const char* n); 
+void detruire_etudiant(etudiant* e); 
+
+#endif // ETUDIANT_H
---
+++ src/etudiant.cpp
@@ -0,0 +1,36 @@
+﻿#include "../include/etudiant.hpp"
+#include <cstring> 
+
+
+void initialiser_etudiant(etudiant* e, int mat, int a, const char* n) {
+    if (e != nullptr) {
+        e->matricule = mat;
+        e->age = a;
+        std::strncpy(e->nom, n, 49);
+        e->nom[49] = '\0';
+    }
+}
+
+void afficher_etudiant(const etudiant* e) {
+    if (e != nullptr) {
+        std::cout << "Matricule: " << e->matricule 
+                  << " | Age: " << e->age 
+                  << " | Nom: " << e->nom << std::endl;
+    }
+}
+
+
+etudiant* creer_etudiant(int mat, int a, const char* n) {
+    etudiant* nouveau_e = new etudiant; 
+    if (nouveau_e != nullptr) {
+        initialiser_etudiant(nouveau_e, mat, a, n);
+    }
+    return nouveau_e;
+}
+
+
+void detruire_etudiant(etudiant* e) {
+    if (e != nullptr) {
+        delete e; 
+    }
+}
